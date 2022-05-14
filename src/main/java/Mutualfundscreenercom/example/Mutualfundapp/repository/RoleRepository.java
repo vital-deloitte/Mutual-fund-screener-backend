ab858c9437e25e9c35344fb5719827ca466b4be1
vital-deloitte <101543922+vital-deloitte@users.noreply.github.com>
2022-05-14 21:05:22 +0530

    Delete src/main/java/Mutualfundscreenercom/example/Mutualfundapp directory

--- src/main/java/Mutualfundscreenercom/example/Mutualfundapp/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package Mutualfundscreenercom.example.Mutualfundapp.repository;
-
-import Mutualfundscreenercom.example.Mutualfundapp.entities.Roles;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RoleRepository extends CrudRepository<Roles, Integer> {
-    Roles findByName(String name);
-}
